3d5ebfc5baac85ef4be6f6bf629df18be64982ba
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-10-31 20:22:57 +0100

    Initial Commit

---
+++ LEMME OPEN IT!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LEMME OPEN IT!
+//
+//  Created by Tim on 31.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LEMME OPEN IT!/LEMME_OPEN_IT_App.swift
@@ -0,0 +1,17 @@
+//
+//  LEMME_OPEN_IT_App.swift
+//  LEMME OPEN IT!
+//
+//  Created by Tim on 31.10.25.
+//
+
+import SwiftUI
+
+@main
+struct LEMME_OPEN_IT_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
